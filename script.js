ae1b4046ffdc7de1b6cadb2bc5b56dfdae7be0d7
Nitin shukla <nitin.shukla12004@gmail.com>
2024-08-01 12:54:44 +0530

    Worked on showing the matched boxes for a particular win

--- script.js
+++ script.js
@@ -16,7 +16,8 @@ let winSound = new Audio("win.wav");
 let divsOfplay = document.querySelectorAll(".item");
 let playTurn = document.querySelector(".turn");
 let reset = document.querySelector(".reset");
-let gifElement = document.querySelector(".gif");
+let imgElement = document.getElementsByTagName("img");
+let line = document.querySelector(".line");
 
 for(i = 0; i<divsOfplay.length; i++){
     divsOfplay[i].addEventListener("click", function(event){
@@ -35,7 +36,9 @@ for(i = 0; i<divsOfplay.length; i++){
         }
         if((matrix[0][0] == matrix[0][1] && matrix[0][1] == matrix[0][2]) || (matrix[0][0] == matrix[1][0] && matrix[1][0] == matrix[2][0]) || (matrix[2][0] == matrix[2][1] && matrix[2][1] == matrix[2][2]) || (matrix[0][2] == matrix[1][2] && matrix[1][2] == matrix[2][2]) || (matrix[0][0] == matrix[1][1] && matrix[1][1] == matrix[2][2]) || (matrix[0][2] == matrix[1][1] && matrix[1][1] == matrix[2][0]) || (matrix[1][0] == matrix[1][1] && matrix[1][1] == matrix[1][2]) || (matrix[0][1] == matrix[1][1] && matrix[1][1] == matrix[2][1])){
             winSound.play();
-            gifElement.style.visibility = "visible";
+            imgElement[0].style.width = "100%";
+            line.style.width = "270px"
+            line.style.transform = "translate(5px, 137px)rotate(45deg)";
         }
     });
 }
@@ -43,10 +46,11 @@ for(i = 0; i<divsOfplay.length; i++){
 reset.addEventListener("click", function(){
     normalise(matrix);
     playsound.play();
+    line.style.width = "0";
+    imgElement[0].style.width = "0";
     for(i = 0; i<divsOfplay.length; i++){
         divsOfplay[i].innerHTML = "";
     }
-    gifElement.style.visibility = "hidden";
     turn = "X";
     playTurn.innerHTML = "Turn: X";
 });
