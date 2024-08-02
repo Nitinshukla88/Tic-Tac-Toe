660f5ca8cbc46463c857be9f4d6803cf3e876bb2
Nitin shukla <nitin.shukla12004@gmail.com>
2024-08-02 11:27:31 +0530

    Game is finished completely. Done the project

--- script.js
+++ script.js
@@ -3,19 +3,22 @@ let validator = 0;
 
 let matrix = [[1, 2, 3],[4, 5, 6],[7, 8, 9]];
 
-function normalise(mat){
+function normalise(mat, conBoxes){
     let start = 1;
     for(i = 0; i<mat.length; i++){
         for(j = 0; j<mat[i].length; j++){
             mat[i][j] = start;
+            conBoxes[start-1].style.backgroundColor = "black";
             start++;
         }
+        
     }
 }
 
 let playsound = new Audio("click.wav");
 let winSound = new Audio("win.wav");
 let drawSound = new Audio("gameover.wav");
+console.log(drawSound);
 let divsOfplay = document.querySelectorAll(".item");
 let playTurn = document.querySelector(".turn");
 let reset = document.querySelector(".reset");
@@ -24,15 +27,16 @@ let line = document.querySelector(".line");
 let message = document.querySelector(".wonMsg");
 
 function mainlogic(tx, ty, angle, winner){
+    playTurn.innerHTML = "";
     winSound.play();
     imgElement[0].style.width = "100%";
     line.style.width = "270px"
     line.style.transform = `translate(${tx}px, ${ty}px) rotate(${angle}deg)`;
     message.style.fontSize = "3rem";
     if(turn == "X"){
-        message.innerHTML = "Hurrah! 0 has won the Game";
+        message.innerHTML = "Hurrah! 0 has won the Game:)";
     }else{
-        message.innerHTML = "Hurrah! X has won the Game";
+        message.innerHTML = "Hurrah! X has won the Game:)";
     }
 }
 
@@ -42,14 +46,14 @@ for(i = 0; i<divsOfplay.length; i++){
         playsound.play();
         if(turn == "X"){
             event.target.innerHTML = "X";
-            event.target.style.backgroundColor = "rgb(11, 11, 11)";
+            event.target.style.backgroundColor = "rgb(13, 13, 13)";
             matrix[indexes[0]][indexes[1]] = "X";
             playTurn.innerHTML = "Turn: 0";
             turn = 0;
             validator++;
         }else{
             event.target.innerHTML = 0;
-            event.target.style.backgroundColor = "rgb(11, 11, 11)";
+            event.target.style.backgroundColor = "rgb(13, 13, 13)";
             matrix[indexes[0]][indexes[1]] = 0;
             playTurn.innerHTML = "Turn: X";
             turn = "X";
@@ -75,7 +79,7 @@ for(i = 0; i<divsOfplay.length; i++){
 
         if(validator == 9){
             drawSound.play();
-            message.innerHTML = "Oops! khichdi pak gyi";
+            message.innerHTML = "Oops! khichdi pak gyi:(";
             message.style.fontSize = "3rem";
         }
     });
@@ -84,7 +88,7 @@ for(i = 0; i<divsOfplay.length; i++){
 reset.addEventListener("click", function(){
     message.style.fontSize = "0";
     validator = 0;
-    normalise(matrix);
+    normalise(matrix, divsOfplay);
     playsound.play();
     line.style.width = "0";
     imgElement[0].style.width = "0";
